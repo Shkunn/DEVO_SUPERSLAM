0679721187ec0008e9c9f537978059f46786ea69
thomasraynal98 <thomas-jean-yvon.raynal@edu.devinci.fr>
2021-03-19 14:02:07 +0100

    secondpushtonpere

--- ORB_SLAM3/include/LoopClosing.h
+++ ORB_SLAM3/include/LoopClosing.h
@@ -11,7 +11,7 @@
 * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
 * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
-*
+* VINCENT EST UNE GROSSE SALOPE
 * You should have received a copy of the GNU General Public License along with ORB-SLAM3.
 * If not, see <http://www.gnu.org/licenses/>.
 */
